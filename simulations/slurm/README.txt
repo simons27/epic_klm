fed99ec799e8207fa2dee03d95751bd6b5d5549f
Rowan Kelleher <rck32@dcc-login-04.oit.duke.edu>
2024-05-24 15:31:51 -0400

    adjusted makeJobs.sh script so that only the eic path needs to be set and all other paths are set from it; also updated README.txt to provide instructions

--- simulations/slurm/README.txt
+++ simulations/slurm/README.txt
@@ -1,6 +1,13 @@
 Simple slurm script for running ddsim simulations with epic_klmws.xml geometry.
 
-To run:
+To run many simulations:
+1. Adjust eic path to fit your setup
+2. adjust command line parameters like number of particles and particle species
+3. run ./makeJobs.sh (should create any required directories)
+4. run ./runJobs.sh
+Note: you may have to adjust sbatch commands to fit your setup like the account name or partition
+
+To run one simulation:
 1. Edit the job.sh script to have correct paths instead of /path/to/...
 2. Create output and error directories (e.g. /work_eic/slurm/output/ and /work_eic/slurm/error/) so they match the slurm directive
 3. Adjust the ddsim command (# of particles, output directory)
--- simulations/slurm/makeJobs.sh
+++ simulations/slurm/makeJobs.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 current_date=$(date +"%B_%d")
-workdir="/path/to/eic/work_eic"
+# EDIT THIS PATH
+eicdir="/path/to/eic"
+#EDIT ABOVE PATH (rest of the paths are based on this)
+epic_klm_dir="${eicdir}/epic_klm"
+slurmdir="${epic_klm_dir}/simulations/slurm"
 
-slurm_output="${workdir}/root_files/Slurm"
-daydir="${slurm_output}/${current_date}"
+output_dir="${eicdir}/simulations/root_files"
+slurm_output_dir="${output_dir}/slurm"
+daydir="${slurm_output_dir}/${current_date}"
 #USER SET VALUES
 outputdir="${daydir}/Run_0/"
-
-out_folder="/path/to/eic/work_eic/slurm/output/output${current_date}"
-error_folder="/path/to/eic/work_eic/slurm/error/error${current_date}"
+standard_output_dir="${slurmdir}/output"
+standard_error_dir="${slurmdir}/error"
+out_folder="${slurmdir}/output/output${current_date}"
+error_folder="${slurmdir}/error/error${current_date}"
 
 rootname="file_"
-processdir="/path/to/eic/epic_klm/"
-runJobs="${workdir}/slurm/runJobs.sh"
+runJobs="${slurmdir}/runJobs.sh"
 touch $runJobs
 chmod +x $runJobs
 echo " " > $runJobs
@@ -35,9 +40,17 @@ if [ ! -d "$error_folder" ]; then
   mkdir "$error_folder"
 fi
 
+if [ ! -d "$standard_output_dir" ]; then
+  mkdir "$standard_output_dir"
+fi
+
+if [ ! -d "$standard_error_dir" ]; then
+  mkdir "$standard_error_dir"
+fi
+
 for num in $(seq 1 40)
 do
-    file="${workdir}/slurm/shells/${rootname}${i}.sh"
+    file="${slurm_output_dir}/shells/${rootname}${i}.sh"
     touch $file
     content="#!/bin/bash\n"
     content+="#SBATCH --account=vossenlab\n"
@@ -46,12 +59,12 @@ do
     content+="#SBATCH --job-name=${rootname}${i}\n"
     content+="#SBATCH --cpus-per-task=1\n"
     content+="#SBATCH --time=24:00:00\n"
-    content+="#SBATCH --chdir=/path/to/eic/epic_klm\n"
+    content+="#SBATCH --chdir=${epic_klm_dir}\n"
     content+="#SBATCH --output=${out_folder}/%x.out\n"
     content+="#SBATCH --error=${error_folder}/%x.err\n"
-    content+="cat << EOF | /path/to/eic/eic-shell\n"
+    content+="cat << EOF | ${eicdir}/eic-shell\n"
     content+="source install/setup.sh\n"
-    content+="/usr/local/bin/ddsim --steeringFile simulations/steering/npsim_local3.py --compactFile /path/to/eic/epic_klm/epic_klmws_only.xml -G -N 30 --gun.particle \"pi-\" --outputFile ${outputdir}pi_5GeV_30events_run_0_file_${i}.edm4hep.root --part.userParticleHandler=\"\"\n"
+    content+="/usr/local/bin/ddsim --steeringFile simulations/steering/npsim_local3.py --compactFile ${epic_klm_dir}/epic_klmws_only.xml -G -N 30 --gun.particle \"pi-\" --outputFile ${outputdir}pi_5GeV_30events_run_0_file_${i}.edm4hep.root --part.userParticleHandler=\"\"\n"
     content+="EOF\n"
     echo -e "$content" > $file 
     echo "sbatch shells/${rootname}${i}.sh" >> $runJobs
